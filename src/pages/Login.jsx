0c149484c878ffff12b7dbea5dce1e1be2467a98
Ankit787762 <ankitsawami6@gmail.com>
2026-06-07 16:54:10 +0530

    Enhance Login and Signup pages: add navigation for logged-in users, improve layout and styling, and include additional input fields for user details.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate,Navigate } from "react-router-dom";
 
 
 function Login(){
+
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+  if (isLoggedIn === "true") {
+  return <Navigate to="/Productpage" replace />;
+  }
 
     const navigate=useNavigate();
 
@@ -14,7 +20,6 @@ function Login(){
 
     const [email,setEmail] =useState("");
     const [password,setPassword] =useState("");
-    const [submit,setSubmit]= useState("");
  
     const Emailcheck=(e)=>{
       setEmail(e.target.value);
@@ -23,18 +28,18 @@ function Login(){
       setPassword(e.target.value);
     }
     const handlechange=(e)=>{
-      //stop page reload when click on submit.
+      
       console.log("email:",email);
       console.log("password:",password);
 
-       if(email==""|| password==""){
+       if(email===""|| password===""){
         alert("enter input fields!");
         return ;
        }
        if(user.email===email&&user.password===password){
         localStorage.setItem("isLoggedIn", "true");
         alert("Login Successful");
-        navigate("/Productpage" )
+        navigate("/Productpage",{ replace: true } )
        }
        else{
         alert("User not found!");
@@ -42,38 +47,60 @@ function Login(){
       }
 
 
-    return(
+return (
+  <div className="min-h-screen flex">
 
-   <div className="min-h-screen flex justify-center items-center bg-gray-300">
-    <div className="flex flex-col items-center gap-4 bg-white p-8 rounded-2xl shadow-2xl w-full max-w-md">
-    {/* Login content */}
-    <div>
-    <h1 className="text-2xl font-bold text-gray-800"> Login </h1>
-    </div>
+    {/* Left Side */}
+    <div className="w-1/2 bg-white  flex flex-col justify-center px-16">
+      <h1 className=" text-blue-600 text-5xl font-bold mb-4">ShopKart</h1>
 
-    <div className="w-full">
-    <h2 className="text-gray-700 font-medium mb-1">Email:</h2>
-    <input onChange={Emailcheck} type="text" placeholder="Enter your Email" className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-     </div>
+      <h2 className="text-2xl font-semibold mb-4">
+        Shop smarter with confidence
+      </h2>
 
-     <div className="w-full">
-     <h2 className="text-gray-700 font-medium mb-1">Password:</h2>
-     <input onChange={Passwordcheck} type="password" placeholder="Enter your Password" className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-      </div>
-
-    <div>
-     <button onClick={handlechange} type="submit" className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded-lg transition duration-300"> Submit</button>
-     </div>
- 
-    <div>
-    <p className="text-gray-600"> Not Registered?{" "}
-        <Link to="/Signuppage" className="text-blue-600 hover:underline"> Create an account</Link>
+      <p className="text-lg  max-w-md">
+        Discover premium electronics, trusted brands, and exclusive deals.
+        Sign in to manage orders and continue your shopping journey.
       </p>
     </div>
+
+    {/* Right Side */}
+    <div className="w-1/2 flex justify-center items-center bg-gray-100">
+
+      <div className="flex flex-col items-center gap-4 bg-white p-8 rounded-2xl shadow-2xl w-full max-w-md">
+
+        <div>
+          <h1 className="text-2xl font-bold text-blue-500">Welcome back</h1>
+          <p>Sign in to continue</p>
+        </div>
+
+        <div className="w-full">
+          <h2 className="text-gray-700 font-medium mb-1"> Email:</h2>
+          <input  onChange={Emailcheck} type="email" placeholder="Enter your Email" className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+        </div>
+
+        <div className="w-full">
+          <h2 className="text-gray-700 font-medium mb-1">Password:</h2>
+          <input  onChange={Passwordcheck} type="password" placeholder="Enter your Password"
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+           />
+        </div>
+
+        <button
+          onClick={handlechange}
+          className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded-lg transition duration-300">
+          Sign In
+        </button>
+
+        <p className="text-gray-600">Not Registered?{" "}
+          <Link to="/Signuppage"className="text-blue-600 hover:underline">Create an account</Link>
+        </p>
+
+      </div>
+    </div>
+
   </div>
-</div>
- 
-    )
+);
 }
 
 export default Login;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,42 +1,90 @@
 import { Link } from "react-router-dom";
 
+
+
+
 function Signup(){
-    return (
 
-   <div className="min-h-screen flex justify-center items-center bg-gray-300">
-    <div className="flex flex-col items-center gap-4 bg-white p-8 rounded-2xl shadow-2xl w-full max-w-md">
 
-     <div>
-    <h1 className="text-2xl font-bold text-gray-800">  SignUp</h1>
-    </div>
 
-    <div className="w-full">
-    <h2 className="text-gray-700 font-medium mb-1">Name:</h2>
-    <input type="text" placeholder="Enter your Name" className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-     </div>
 
-    <div className="w-full">
-    <h2 className="text-gray-700 font-medium mb-1">Email:</h2>
-    <input type="text" placeholder="Enter your Email" className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-     </div>
+  
+  return (
 
-     <div className="w-full">
-     <h2 className="text-gray-700 font-medium mb-1">Password:</h2>
-     <input type="password" placeholder="Enter your Password" className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-      </div>
-
-    <div>
-     <button className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded-lg transition duration-300"> Submit</button>
-     </div>
+    <div className="min-h-screen flex">
  
-    <div>
-    <p className="text-gray-600"> Already have an account?{" "}
-        <Link to="/Loginpage" className="text-blue-600 hover:underline"> login</Link>
+    
+    <div className="w-1/2 bg-white  flex flex-col justify-center px-10">
+     <h1 className="text-blue-600 text-5xl font-bold mb-4">
+      Join ShopKart
+     </h1>
+
+     <h2 className="text-2xl font-semibold mb-4">
+       Start your tech journey today
+     </h2>
+
+     <p className="text-lg max-w-md">
+      Create your account to unlock exclusive offers,
+      faster checkout, and a personalized shopping
+      experience.
       </p>
     </div>
 
-   </div>  
-    </div> 
+    
+     <div className="w-1/2 flex justify-center items-center bg-gray-100">
+      <div className="flex flex-col items-center gap-4 bg-white p-8 rounded-2xl shadow-2xl w-full max-w-md">
+
+      <div>
+        <h1 className="text-2xl font-bold text-blue-500">Create Account</h1>
+      </div>
+
+      <div className="flex justify-between gap-4">
+      <div className="w-full">
+        <h2 className="text-gray-700 font-medium mb-1">First Name:</h2>
+        <input type="text"placeholder="Enter First Name"
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+      </div>
+
+            <div className="w-full">
+        <h2 className="text-gray-700 font-medium mb-1">Last Name:</h2>
+        <input type="text"placeholder="Enter Last Name"
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+      </div>
+      </div>
+
+
+      <div className="w-full">
+        <h2 className="text-gray-700 font-medium mb-1"> Email:</h2>
+
+        <input type="text" placeholder="Enter your Email"
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+
+      <div className="w-full">
+        <h2 className="text-gray-700 font-medium mb-1"> Password: </h2>
+        <input type="password" placeholder="Enter your Password"
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+
+       <div className="w-full">
+        <h2 className="text-gray-700 font-medium mb-1">Confirm Password: </h2>
+        <input type="password" placeholder="Enter your Password"
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+
+      <button className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded-lg transition duration-300"> Submit </button>
+
+      <p className="text-gray-600"> Already have an account?{" "}
+        <Link to="/Loginpage" className="text-blue-600 hover:underline"> Login</Link>
+      </p>
+
+    </div>
+  </div>
+
+</div>
 
     )
 }
