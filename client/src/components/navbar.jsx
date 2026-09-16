6f82f82de3606b4f30019ab48252ee243db85f0a
Ankit787762 <ankitsawami6@gmail.com>
2026-09-16 20:24:01 +0530

    feat: implement user state management in Navbarpage for dynamic rendering

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Api from "../services/Api";
 
 function Navbarpage() {
     const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const checkUser = async () => {
+            try {
+                const res = await Api.get("/users/me");
+                setUser(res.data.user);
+            } catch (error) {
+                setUser(null);
+            }
+        };
+
+        checkUser();
+    }, []);
 
     const handlechange = async () => {
         try {
             await Api.post("/users/logout");
+            setUser(null);
             navigate("/", { replace: true });
         } catch (error) {
             console.log(error);
@@ -18,12 +34,17 @@ function Navbarpage() {
             <h1 className="text-2xl font-bold">ShopKart</h1>
 
             <div className="flex justify-center items-center gap-20 mr-20">
-                <Link to="/">Home</Link>
-                <Link to="/Aboutpage">About</Link>
-
-                <button onClick={handlechange}>
-                    Logout
-                </button>
+                {user ? (
+                    <button onClick={handlechange}>
+                        Logout
+                    </button>
+                ) : (
+                    <>
+                        <Link to="/">Home</Link>
+                        <Link to="/Aboutpage">About</Link>
+                        <Link to="/Loginpage">Login</Link>
+                    </>
+                )}
             </div>
         </div>
     );
